1eeb5af30dc598212f09ae94594c1ec0997874bb
kevinjaniky <kevin.janiky@gmail.com>
2023-09-12 18:02:16 +0200

    Add validated content only

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -66,6 +66,32 @@ class PublicController extends AbstractController
         ]);
     }
 
+    #[Route('/validated', name: 'app_public_validated')]
+    public function validated(Request $request, CustomerSecurityService $customerSecurityService, EntityManagerInterface $entityManager): Response
+    {
+        $client = $customerSecurityService->checkAccess($request->query->get('token'));
+        if (empty($client)) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $publications = $entityManager->getRepository(Publication::class)->findBy([
+            'client' => $client,
+            'state' => 'validate'
+        ], [
+            'publishedAt' => 'DESC'
+        ]);
+
+        $publications = array_filter($publications, function ($publication) {
+            return $publication->getState() !== 'draft';
+        });
+
+        return $this->render('public/index.html.twig', [
+            'publications' => $publications,
+            'client' => $client,
+            'page' => 'all'
+        ]);
+    }
+
     #[Route('/{token}/action/{id}', name: 'app_public_action')]
     public function action($token, Publication $publication, Request $request, CustomerSecurityService $customerSecurityService, EntityManagerInterface $entityManager, MailerInterface $mailer): JsonResponse
     {
